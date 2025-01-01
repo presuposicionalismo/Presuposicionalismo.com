a66bb657916c0e689897c9b8772b28e3719e72a3
Luis Reyes. <reyesdelosreyes.1997@gmail.com>
2025-01-01 01:07:53 -0500

    add book Junius and van til, and rebuild

--- script/rename.py
+++ script/rename.py
@@ -7,11 +7,11 @@ root_path = os.path.expanduser("../src/content/blog")
 for root, dirs, files in os.walk(root_path):
     for file in files:
         # Cambia solo los archivos que terminan en .md
-        if file.endswith(".md"):
+        if file.endswith(".mdx"):
             # Obtén el nombre completo del archivo
             old_name = os.path.join(root, file)
             # Cambia la extensión a .mdx
-            new_name = old_name[:-3] + ".mdx"
+            new_name = old_name[:-3] + ".md"
             # Renombra el archivo
             os.rename(old_name, new_name)
             print(f"Renombrado: {old_name} -> {new_name}")
