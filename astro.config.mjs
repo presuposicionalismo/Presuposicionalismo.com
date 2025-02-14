a940cbf16b0dc51f012be1c0ab23d9c4c1f4f1c2
Luis Reyes. <reyesdelosreyes.1997@gmail.com>
2025-02-14 16:19:31 -0500

    add tailwind 4 integration
    add tailwind 4 integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,21 +7,34 @@ import fuse from "astro-fuse";
 
 import svelte from "@astrojs/svelte";
 
+
+import tailwindcss from "@tailwindcss/vite";
+
+
 // remarkPlugins: [remarkToc],
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://presuposicionalismo.com",
+
   integrations: [mdx({
     gfm: true,
-  }), sitemap(), fuse(['content']), svelte()],
+  }), sitemap(), fuse(['content']), svelte()
+],
+
   markdown: {
     remarkPlugins: [],
     gfm: true,
   },
+
   output: "static",
   adapter: vercel(),
+
   webAnalytics: {
     enabled: true,
   },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
