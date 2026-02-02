39da739c362b74d3dd872f1f5effe75e1d6c6a02
presuposicionalismo <presuposicionalismoapologetica@gmail.com>
2026-02-02 13:55:46 -0500

    chore: remove style blocks and legacy classes from components

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -26,38 +26,3 @@
     </div>
   {/each}
 </nav>
-
-<style>
-  @import "../styles/global.css";
-
-  a {
-    cursor: pointer;
-    display: flex;
-    gap: 0.5rem;
-	padding: var(--size-fluid-1);
-
-    transition: all 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
-    border-radius: var(--radius-3);
-    place-content: center;
-	font-size: var(--font-size-base);
-	font-weight: var(--font-bold);
-    line-height: 1rem;
-
-    background-color: var(--surface-2);
-	color: var(--text-1);
-  }
-  a:hover {
-    background-color: var(--surface-3);
-	transform: translate(0, -0.25rem);
-  }
-
-  div {
-    display: inline-block;
-    margin-right: 1rem;
-  }
-
-  .active {
-    font-weight: bold;
-    text-decoration: underline;
-  }
-</style>
