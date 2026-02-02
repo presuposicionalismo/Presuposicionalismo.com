ba0734af17f41a31d830b2150db97257cb12b811
presuposicionalismo <presuposicionalismoapologetica@gmail.com>
2026-02-02 13:48:44 -0500

    chore: reset css to baseline (remove all custom styles)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,6 @@
-const OpenProps = require("open-props");
-const postcssJitProps = require("postcss-jit-props");
 const postcssNesting = require("postcss-nesting");
 const postcssCustomMedia = require("postcss-custom-media");
 
 module.exports = {
-  plugins: [postcssNesting(), postcssCustomMedia(), postcssJitProps(OpenProps)],
+  plugins: [postcssNesting(), postcssCustomMedia()],
 };
